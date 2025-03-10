1d1ef8cc842f44aef2d3ab2dc37335533b3f9d70
Pablo Mora <panastacio@dotdashmdp.com>
2025-03-09 21:40:19 -0600

    debug java.library.path

---
+++ README.md
@@ -0,0 +1,10 @@
+## ReadME
+
+### Issues:
+
+*Maven – Fatal error compiling: invalid target release*
+
+Export JAVA_HOME to an appropriate Java SDK
+```
+export JAVA_HOME=/home/pablo/.jdks/openjdk-23.0.2
+```
--- src/main/java/com/signatureGuard/ImageReader.java
+++ src/main/java/com/signatureGuard/ImageReader.java
@@ -4,23 +4,63 @@ import javax.imageio.ImageIO;
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorConvertOp;
+import java.awt.image.DataBufferByte;
 import java.io.File;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Core;
+
 
 public class ImageReader {
 
-
-    public static void main(String[] args) {
-        BufferedImage signatureA = loadImage("firma1.png");
-        BufferedImage signatureB = loadImage("firma2.png");
-        if (signatureA != null && signatureB != null) {
-            signatureA = convertToGrayscale(signatureA);
-            signatureB = convertToGrayscale(signatureB);
-        }
-        double signatureSimilarity = compareSignatures(signatureA, signatureB);
+    static {
+        System.out.println("Core version: " + Core.NATIVE_LIBRARY_NAME);
+        System.out.println(System.getProperty("java.library.path"));
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    private static double compareSignatures(BufferedImage signatureA, BufferedImage signatureB) {
-        return 0;
+
+    public static void main(String[] args) {
+        System.out.println("OpenCV version: " + Core.VERSION);
+        String signatureAPath = "firmas/firma1.png";
+        String signatureBPath = "firmas/firma2.png";
+        BufferedImage signatureA = loadImage(signatureAPath);
+        BufferedImage signatureB = loadImage(signatureBPath);
+        if (signatureA != null && signatureB != null) {
+//            signatureA = convertToGrayscale(signatureA);
+//            signatureB = convertToGrayscale(signatureB);
+            double signatureSimilarity = compareSignatures(signatureAPath, signatureBPath);
+            System.out.println("The similarity between the signatures is: " + (signatureSimilarity * 100) + "%");
+        } else {
+            System.out.println("Error loading images.");
+        }
+    }
+
+    private static double compareSignatures(String signatureA, String signatureB) {
+        Mat img1 = Imgcodecs.imread(signatureA, Imgproc.COLOR_BGR2GRAY);
+        Mat img2 = Imgcodecs.imread(signatureB, Imgproc.COLOR_BGR2GRAY);
+
+        Imgproc.GaussianBlur(img1, img1, new Size(5, 5), 0);
+        Imgproc.GaussianBlur(img2, img2, new Size(5, 5), 0);
+
+        Imgproc.Canny(img1, img1, 50, 150);
+        Imgproc.Canny(img2, img2, 50, 150);
+
+        Mat diff = new Mat();
+        Core.absdiff(img1, img2, diff);
+
+        Scalar sumDiff = Core.sumElems(diff);
+        double difference = sumDiff.val[0] / (img1.rows() * img1.cols());
+
+        return 1 - (difference / 255.0);
+    }
+
+    private static Mat bufferedImageToMat(BufferedImage img) {
+        byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+        Mat mat = new Mat(img.getHeight(), img.getWidth(), CvType.CV_8UC1);
+        mat.put(0, 0, pixels);
+        return mat;
     }
 
     public static BufferedImage loadImage(String path) {
