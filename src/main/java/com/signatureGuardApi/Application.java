92f8edca780968dab96359de2974aeacec70d5c6
Pablo Mora <panastacio@dotdashmdp.com>
2025-04-01 22:29:24 -0600

    WIP migrate application logic to support spring boot api

---
+++ src/main/java/com/signatureGuardApi/Application.java
@@ -0,0 +1,14 @@
+package com.signatureGuardApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.signatureGuard.Main.class, args);
+    }
+
+}
